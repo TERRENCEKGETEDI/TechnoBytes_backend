753d546d8d8896a59a3e6ee7d445977a9e3b2485
Terrence Kgetedi <terrencekgetedi@gmail.com>
2025-09-28 05:07:04 +0200

    tokens

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_secret')
-    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 900))  # 15 minutes
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 31536000))  # 1 year
     JWT_REFRESH_TOKEN_EXPIRES = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES', 2592000))  # 30 days
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
     HOME_AFFAIRS_API_BASE = os.getenv('HOME_AFFAIRS_API_BASE', 'https://api.example.com')
